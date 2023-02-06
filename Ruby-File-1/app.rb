53ba4af79cbbbb7b6df957cb07a8921566594b5f
tugi Ngunjiri <102291617+Tugi-Ngunjiri@users.noreply.github.com>
2023-02-06 11:24:35 +0300

    Add files via upload

---
+++ Ruby-File-1/app.rb
@@ -0,0 +1,19 @@
+#app.rb 
+puts "Hello World"
+# app.rb 
+puts("Hello World!")
+#app.rb
+print"Hello World!"
+print"Hello World!"
+print"Hello World!"
+
+# app.rb
+puts [1, 2, 3]
+
+#This is equivalent of calling:
+#puts [1, 2 ,3 ]. inspect
+p[1, 2 , 3 ]
+
+# app.rb
+pp[{ id: 1, hello "world"} ,{id :2, hello: "Ruby"},{id : 3, hello:"Moon" }.{ id: 4 , hello: "learner"} ]
+
